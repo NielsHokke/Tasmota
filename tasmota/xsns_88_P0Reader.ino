9f0412e8f33bb7ba902c4c4f4ba67281a7b43c39
Niels Hokke <NielsHokke@gmail.com>
2021-08-15 09:38:28 +0200

    successfully reading and displaying data

--- tasmota/xsns_88_P0Reader.ino
+++ tasmota/xsns_88_P0Reader.ino
@@ -4,7 +4,7 @@
 // Define driver ID
 #define XSNS_88  88
 
-#define P0_READER_PERIOD     50         // In 100ms loops
+#define P0_READER_PERIOD     20         // In 250ms loops
 
 #define P0_READER_BAUDRATE   300
 #define P0_RX_BUF_SIZE       230
@@ -12,18 +12,64 @@
 #define P0_MESSAGE_RECEIVED  0x02
 
 #define P0_REQUEST_MSG_SIZE  5
+#define P0_METER_NAME_SIZE   25
 
 #include <TasmotaSerial.h>
 
 struct {
   uint8_t status = 0;
   uint8_t period_count = 0;
+  uint8_t line_count = 0;
   uint8_t new_byte = 0;
   uint16_t byte_counter = 0;
   char request_msg[P0_REQUEST_MSG_SIZE] = {0x2F, 0x3F, 0x21, 0x0D, 0x0A};    // "/?!\r\n"
   char *rx_buf = nullptr;
   TasmotaSerial *serial_handle = nullptr;
 } P0;
+
+struct {
+  char Meter_Name[P0_METER_NAME_SIZE] = "Meter naam";
+  uint32_t Serial_Number = 0;
+  uint32_t Device_Adress = 0;
+  long Total_Pos_Energy = 0;
+  long T1_Pos_Energy = 0;
+  long T2_Pos_Energy = 0;
+  long Total_Neg_Energy = 0;
+  long T1_Neg_Energy = 0;
+  long T2_Neg_Energy = 0;
+  uint32_t Fatal_Error = 0;
+  uint8_t has_data = 0;
+} P0_Data;
+
+/* Subset of OBIS (or EDIS) codes from IEC 62056 provided by the ISKRA ME-162-0012.
+ * [A-B:]C.D.E[*F] where the ISKRA ME-162 does not do [A-B:] medium addressing. */
+
+enum Obis {
+  OBIS_C_1_0 = 0, // Meter serial number
+  OBIS_0_0_0,     // Device address
+  OBIS_1_8_0,     // Positive active energy (A+) total [Wh]
+  OBIS_1_8_1,     // Positive active energy (A+) in tariff T1 [Wh]
+  OBIS_1_8_2,     // Positive active energy (A+) in tariff T2 [Wh]
+  OBIS_2_8_0,     // Negative active energy (A+) total [Wh]
+  OBIS_2_8_1,     // Negative active energy (A+) in tariff T1 [Wh]
+  OBIS_2_8_2,     // Negative active energy (A+) in tariff T2 [Wh]
+  OBIS_F_F_0,     // Fatal error meter status
+  OBIS_LAST
+};
+
+// typedef struct { char pgm_str[6]; } obis_pgm_t;
+const char Obis_[OBIS_LAST + 1][6] = {
+  "C.1.0",
+  "0.0.0",
+  "1.8.0",
+  "1.8.1",
+  "1.8.2",
+  "2.8.0",
+  "2.8.1",
+  "2.8.2",
+  "F.F",
+  "UNDEF",
+};
 
 
 /********************************************************************************************/
@@ -53,7 +99,72 @@ void P0_Reader_Init() {
 void P0_Parse_Line(){
 
   AddLog(LOG_LEVEL_INFO, PSTR("P0: P0_Parse_Line"));
-  AddLogBuffer(LOG_LEVEL_INFO, (uint8_t*) P0.rx_buf, P0.byte_counter);
+  // AddLogBuffer(LOG_LEVEL_INFO, (uint8_t*) P0.rx_buf, P0.byte_counter);
+
+  // Init OBIS_Code and value fields
+  char *c_OBIS_Code = P0.rx_buf;
+  uint8_t OBIS_Code_len = 0;
+  Obis OBIS_Code = OBIS_LAST;
+  char *c_value = P0.rx_buf;
+  uint8_t value_len = 0;
+  long value = 0;
+
+  // Find end of OBIS_Code and start/length of value
+  while(*c_value != '(' && c_value - P0.rx_buf < P0.byte_counter) { c_value++; }
+  OBIS_Code_len = c_value++ - P0.rx_buf;
+
+  // If value field found
+  if(OBIS_Code_len != P0.byte_counter){
+    value_len = P0.byte_counter - OBIS_Code_len - 2; // -2 to omit '(' & ')' 
+  }else{
+    c_value =  P0.rx_buf;
+    value_len = P0.byte_counter;
+  }
+  
+  // AddLogBuffer(LOG_LEVEL_INFO, (uint8_t*) c_OBIS_Code, OBIS_Code_len);
+  // AddLogBuffer(LOG_LEVEL_INFO, (uint8_t*) Obis_[0], OBIS_Code_len);
+
+  // Parse OBIS code
+  if(OBIS_Code_len){
+    for (int i = 0; i < OBIS_LAST; i++) {
+      if (memcmp(c_OBIS_Code, Obis_[i], OBIS_Code_len) == 0){
+        OBIS_Code = (Obis)i;
+        break;
+      }
+    }
+  }
+
+  AddLog(LOG_LEVEL_INFO, PSTR("P0: OBIS %d"), (uint8_t) OBIS_Code);
+
+
+  // First line is meter name without OBIS code
+  if(P0.line_count == 0 && OBIS_Code == OBIS_LAST){
+      memcpy(P0_Data.Meter_Name, c_value, value_len);
+      P0_Data.has_data = 1;
+      return;
+  }
+
+  // Parse value, if in kWh parse to Wh 
+  value = atol(c_value);
+  if (value_len == 15 && c_value[7] == '.' && memcmp_P(c_value + 11, F("*kWh"), 4) == 0) {
+    value = value * 1000 + atol(c_value + 8);
+  }else{
+    return;
+  }
+  
+  // Save in data struct
+  switch (OBIS_Code) {
+    case OBIS_C_1_0: P0_Data.Serial_Number = value; break;
+    case OBIS_0_0_0: P0_Data.Device_Adress = value; break;
+    case OBIS_1_8_0: P0_Data.Total_Pos_Energy = value; break;
+    case OBIS_1_8_1: P0_Data.T1_Pos_Energy = value; break;
+    case OBIS_1_8_2: P0_Data.T2_Pos_Energy = value; break;
+    case OBIS_2_8_0: P0_Data.Total_Neg_Energy = value; break;
+    case OBIS_2_8_1: P0_Data.T1_Neg_Energy = value; break;
+    case OBIS_2_8_2: P0_Data.T2_Neg_Energy = value; break;
+    case OBIS_F_F_0: P0_Data.Fatal_Error = (uint32_t) value; break;
+    default: break;
+  }
 }
 
 void P0_Reader_Tick(){
@@ -68,11 +179,17 @@ void P0_Reader_Tick(){
       yield();
       P0.new_byte = P0.serial_handle->read();
 
+      // Extend period timout
+      if(P0.period_count <= 2){
+        P0.period_count = 5;
+      }
+
       // Wait for message start character
       if(!P0.status & P0_MESSAGE_STARTED){
         if(P0.new_byte == '/'){
           P0.status |= P0_MESSAGE_STARTED;
           P0.byte_counter = 0;
+          P0.line_count = 0;
 
         }else{
           continue;
@@ -97,6 +214,7 @@ void P0_Reader_Tick(){
             // Parse line
             P0_Parse_Line();
             P0.byte_counter = 0;
+            P0.line_count++;
           }
         }
       }
@@ -105,6 +223,8 @@ void P0_Reader_Tick(){
   // Transmit
   if(P0.period_count <= 0){
 
+      AddLog(LOG_LEVEL_INFO, PSTR("P0: New period"));
+
       P0.serial_handle->flush();
 
       // Transmit requist message
@@ -112,19 +232,46 @@ void P0_Reader_Tick(){
           P0.serial_handle->write(P0.request_msg[i]);
       }
 
+      // Reset period counter and status flags
       P0.period_count = P0_READER_PERIOD;
       P0.status &= ~P0_MESSAGE_RECEIVED;
       P0.status &= ~P0_MESSAGE_STARTED;
       
   }else{
+      // Count down period counter
       P0.period_count--;
   }
 }
 
 #ifdef USE_WEBSERVER
+
+// TODO move to language files
+const char HTTP_Meter_ID[] PROGMEM = "{s}Meter ID:  {m}%s{e}";
+// const char HTTP_Serial_Num[] PROGMEM = "{s}Serial number{m}%d{e}";
+// const char HTTP_Device_Adrs[] PROGMEM = "{s}Device adress{m}%d{e}";
+const char HTTP_Total_Pos_Energy[] PROGMEM = "{s}Total positive energy:{m}%d.%03d kWh{e}";
+const char HTTP_T1_Pos_Energy[] PROGMEM = "{s}Positive energy tariff 1:{m}%d.%03d kWh{e}";
+const char HTTP_T2_Pos_Energy[] PROGMEM = "{s}Positive energy tariff 2:{m}%d.%03d kWh{e}";
+const char HTTP_Total_Neg_Energy[] PROGMEM = "{s}Total negative energy:{m}%d.%03d kWh{e}";
+const char HTTP_T1_Neg_Energy[] PROGMEM = "{s}Negative energy tariff 1:{m}%d.%03d kWh{e}";
+const char HTTP_T2_Neg_Energy[] PROGMEM = "{s}Negative energy tariff 2:{m}%d.%03d kWh{e}";
+const char HTTP_Fatel_Error[] PROGMEM = "{s}Fatal error:{m}%d{e}";
+
+
 void P0_Reader_Show(void) {
-  if (!P0.rx_buf) { return; }
-  WSContentSend_PD(PSTR("{s}P0 Reader UID{m}%d {e}"), P0.period_count);
+  if (!P0.serial_handle) { return; }
+
+  if(P0_Data.has_data) WSContentSend_PD(HTTP_Meter_ID, P0_Data.Meter_Name);
+  // if(P0_Data.Serial_Number) WSContentSend_PD(HTTP_Serial_Num, P0_Data.Serial_Number);
+  // if(P0_Data.Device_Adress) WSContentSend_PD(HTTP_Device_Adrs, P0_Data.Device_Adress);
+  if(P0_Data.Total_Pos_Energy) WSContentSend_PD(HTTP_Total_Pos_Energy, P0_Data.Total_Pos_Energy/1000, P0_Data.Total_Pos_Energy%1000);
+  if(P0_Data.T1_Pos_Energy) WSContentSend_PD(HTTP_T1_Pos_Energy, P0_Data.T1_Pos_Energy/1000, P0_Data.T1_Pos_Energy%1000);
+  if(P0_Data.T2_Pos_Energy) WSContentSend_PD(HTTP_T2_Pos_Energy, P0_Data.T2_Pos_Energy/1000, P0_Data.T2_Pos_Energy%1000);
+  if(P0_Data.Total_Neg_Energy) WSContentSend_PD(HTTP_Total_Neg_Energy, P0_Data.Total_Neg_Energy/1000, P0_Data.Total_Neg_Energy%1000);
+  if(P0_Data.T1_Neg_Energy) WSContentSend_PD(HTTP_T1_Neg_Energy, P0_Data.T1_Neg_Energy/1000, P0_Data.T1_Neg_Energy%1000);
+  if(P0_Data.T2_Neg_Energy) WSContentSend_PD(HTTP_T2_Neg_Energy, P0_Data.T2_Neg_Energy/1000, P0_Data.T2_Neg_Energy%1000);
+  if(P0_Data.Fatal_Error) WSContentSend_PD(HTTP_Fatel_Error, P0_Data.Fatal_Error);
+
 }
 #endif  // USE_WEBSERVER
 
@@ -139,7 +286,7 @@ bool Xsns88(byte function) {
     case FUNC_INIT:
       P0_Reader_Init();
       break;
-    case FUNC_EVERY_100_MSECOND:
+    case FUNC_EVERY_250_MSECOND:
       P0_Reader_Tick();
       break;
 #ifdef USE_WEBSERVER
